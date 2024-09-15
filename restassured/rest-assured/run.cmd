8b11e72fd0494bf003bd29aa396608cb9d09e8c8
Syeda1234 <69795425+Syeda1234@users.noreply.github.com>
2024-09-15 21:45:37 +0530

    Initial push
    Initial push

---
+++ restassured/rest-assured/run.cmd
@@ -0,0 +1,1 @@
+javadoc -docletpath target\rest-assured-0.0.1-SNAPSHOT-jar-with-dependencies.jar -doclet io.rest_assured.rest_assured.Doclet %*
---
+++ restassured/rest-assured/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+javadoc -docletpath target/rest-assured-0.0.1-SNAPSHOT-jar-with-dependencies.jar -doclet io.rest_assured.rest_assured.Doclet "$@"
